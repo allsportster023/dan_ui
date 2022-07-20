7cfcb818c58180017688ec8fad6e075920ebe61e
Ben Slaughter <bslaughter@icr-team.com>
2022-07-19 21:42:45 -0600

    Added initial Map and fixed some devops stuff for Windows

---
+++ dan-app/src/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+    return (
+        <div className="NavBar_heading">
+            <h1>Threat Control Tool</h1>
+        </div>
+    );
+}
+
