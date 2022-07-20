555b895e515660651bc677d063a43e133de93b9d
Ben Slaughter <bslaughter@icr-team.com>
2022-07-20 14:41:38 -0600

    Added websockets and started updating the location of the aircraft based on the data. Merged changes from Dan

--- dan-app/src/App.js
+++ dan-app/src/App.js
@@ -4,25 +4,36 @@ import axios from 'axios';
 import { ThreatGrid } from './components/ThreatGrid'
 import NavBar from "./NavBar";
 import Map from "./Map";
-import {  ThemeProvider,
-  createTheme} from '@mui/material'
-
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import {  ThemeProvider, createTheme} from '@mui/material'
 import baseTheme from "./muiThemes/baseTheme.js";
 
-
+const client = new W3CWebSocket('ws://localhost:6969');
 const baseUrl = "http://localhost:8080/";
 
 function App() {
 
   const [theme, setTheme] = useState(baseTheme);
-
-
   const [threatsError, setThreatsError] = useState(null)
   const [threatsLoading, setThreatsLoading] = useState(true)
   const [threats, setThreats] = useState([])
   const [posRepsError, setPosRepError] = useState(null)
   const [posRepsLoading, setPosRepLoading] = useState(true)
-  const [posReps, setPosReps] = useState([])
+  const [posReps, setPosReps] = useState(null)
+
+  client.onopen = () => {
+    console.log('WebSocket Client Connected');
+  };
+
+  //Message will be an array of position reports for each aircraft
+  client.onmessage = (message) => {
+
+    if(message.data.lat !== null){
+      setPosReps([JSON.parse(message.data)])
+    }
+
+    client.send("Hi")
+  };
 
   useEffect(() => {
     axios
@@ -59,10 +70,9 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-    
     <div className="App">
       <NavBar />
-        {!threatsError && !threatsLoading && <Map threats={threats}/>}
+          {!threatsError && !threatsLoading && <Map threats={threats} posReps={posReps}/>}
         {!threatsError && !threatsLoading &&
         <ThreatGrid
           threats={threats}
--- dan-app/src/Map.js
+++ dan-app/src/Map.js
@@ -4,13 +4,16 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
 import './Map.css';
 
-export default function Map( {threats} ) {
+export default function Map({threats, posReps}) {
+
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng] = useState(-76.11);
-    const [lat] = useState(37.01);
-    const [zoom] = useState(8);
+    const [lat] = useState(37.21);
+    const [zoom] = useState(7);
     const [API_KEY] = useState('Ce9hgYWIkaeo6JSNYZbf');
+
+    let aircraftMarkerArr = [];
 
     useEffect(() => {
         if (map.current) return;
@@ -23,22 +26,91 @@ export default function Map( {threats} ) {
 
         map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
 
-        {threats.map((threat) => {
+//         map.current.on('mousemove', function (e) {
+//             document.getElementById('info').innerHTML =
+// // e.point is the x, y coordinates of the mousemove event relative
+// // to the top-left corner of the map
+//
+//                 // e.lngLat is the longitude, latitude geographical position of the event
+//                 JSON.stringify(e.lngLat.wrap());
+//         });
 
-            var threatIcon = document.createElement('div');
-            threatIcon.classList.add('Map_SamMarker');
+        map.current.once("load", () => {
+            // This code runs once the base style has finished loading.
 
-            // threatIcon.addEventListener('click', function () {
-            //     window.alert(threat.name);
-            // });
+            map.current.addSource('maine', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Polygon',
+                        'coordinates': [
+                            [
+                                [-76.41, 36.81],
+                                [-75.75, 37.27],
+                                [-75.50, 37.05]]
+                        ]
+                    }
+                }
+            })
 
-            new maplibregl.Marker(threatIcon)
-                .setLngLat([threat.longitude, threat.latitude])
-                .addTo(map.current);
+            map.current.addLayer({
+                'id': 'maine',
+                'type': 'fill',
+                'source': 'maine',
+                'layout': {},
+                'paint': {
+                    'fill-color': '#088',
+                    'fill-opacity': 0.8
+                }
+            });
+        });
 
-        })}
+        {
+            threats.map((threat) => {
 
-    });
+                var threatIcon = document.createElement('div');
+                threatIcon.classList.add('Map_SamMarker');
+
+                new maplibregl.Marker(threatIcon)
+                    .setLngLat([threat.longitude, threat.latitude])
+                    .addTo(map.current);
+            })
+        }
+
+    }, [threats]);
+
+    useEffect(() => {
+
+        {
+            posReps.map((posRep) => {
+
+                console.log(aircraftMarkerArr)
+
+                aircraftMarkerArr.map((marker) => {
+
+                    console.log("Removing Marker: ", marker)
+                    marker.remove()
+                })
+
+                var targetIcon = document.createElement('div');
+                targetIcon.classList.add('Map_AircraftMarker');
+
+                console.log("Creating TargetIcon")
+
+                const newMarker = new maplibregl.Marker(targetIcon)
+                    .setLngLat([posRep.lng, posRep.lat])
+                    .setRotation(posRep.hdg)
+                    .addTo(map.current);
+
+                aircraftMarkerArr = []
+                aircraftMarkerArr.push(newMarker)
+
+                console.log(aircraftMarkerArr)
+
+            })
+        }
+    }, [posReps])
 
     return (
         <div className="map-wrap">
